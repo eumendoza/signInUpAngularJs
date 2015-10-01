76f64a7fc213fc6be4497b1331c40e52fdae6b94
Eugenio Mendoza <eu.mendoza@gmail.com>
2015-10-01 16:19:23 +0200

    Se crea el controller para el Registro del usuario y se añade la validación básica

--- scripts/controllers/registerCtrl.js
+++ scripts/controllers/registerCtrl.js
@@ -1,3 +1,8 @@
 angular.module('registerApp').controller('registerCtrl', ['$scope', function($scope) {
-
+	$scope.uregister = {}
+	$scope.register = function(isValid){
+		if(isValid){
+			console.log($scope.uregister);
+		}
+	}
 }]);
